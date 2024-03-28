136e474c04bb70181b74a2cff21e5a4822bf70a0
mosum201 <127705608+mosum201@users.noreply.github.com>
2024-03-28 19:32:52 +0900

    user 페이지 커밋
    '

--- front/src/Pages/User.jsx
+++ front/src/Pages/User.jsx
@@ -2,13 +2,15 @@ import { MdShare, MdAssignment } from "react-icons/md";
 import React, { useState } from "react";
 import Card from "../Components/Card.jsx";
 import { Products } from "../stores/mockData.js";
+import { useParams } from "react-router-dom";
 
 const User = () => {
   const [selectedButton, setSelectedButton] = useState("전체");
   const [selectedTab, setSelectedTab] = useState("최신순");
+  let { id } = useParams();
 
   return (
-    <div className="flex justify-center w-[1280px space-x-8">
+    <div className="flex justify-center w-[1280px] space-x-8 mb-20">
       <div className="flex w-[174px] flex-col">
         <h2 className="text-2xl mb-4">마이 페이지</h2>
         <hr className="border-t border-gray-500 my-2" />
@@ -46,15 +48,15 @@ const User = () => {
               </div>
               <div className="flex justify-between w-full h-[91px] rounded-lg bg-[#EEEEEE]">
                 <div className="h-full w-[calc(100%/3)] border-r border-gray-400 flex items-center justify-center flex-col">
-                  <p className="text-medium text-light-gray">판매중</p>
+                  <p className="text-medium text-[#9CA3AF]">판매중</p>
                   <p className="text-[22px]">0</p>
                 </div>
                 <div className="h-full w-[calc(100%/3)] border-r border-gray-400 flex items-center justify-center flex-col">
-                  <p className="text-medium text-light-gray">판매완료</p>
+                  <p className="text-medium text-[#9CA3AF]">판매완료</p>
                   <p className="text-[22px]">0</p>
                 </div>
                 <div className="h-full w-[calc(100%/3)] flex items-center justify-center flex-col">
-                  <p className="text-medium text-light-gray">구매완료</p>
+                  <p className="text-medium text-[#9CA3AF]">구매완료</p>
                   <p className="text-[22px]">0</p>
                 </div>
               </div>
@@ -62,24 +64,24 @@ const User = () => {
           </div>
         </div>
         <div className="mt-[59px] text-[24px] font-bold">내 상품</div>
-        <div className="mt-[18px] w-full h-[58px] border-b border-light-gray bg-white flex items-center mb-[24px]">
+        <div className="mt-[18px] w-full h-[58px] border-b border-[#9CA3AF] bg-white flex items-center mb-[24px]">
           <button
             className={`w-[160px] h-full text-center border-b-2 border-transparent font-semibold focus:outline-none 
-              ${selectedButton === "전체" ? "text-black border-black" : "text-light-gray"}`}
+              ${selectedButton === "전체" ? "text-black border-black border-b-8" : "text-[#9CA3AF]"}`}
             onClick={() => setSelectedButton("전체")}
           >
             전체
           </button>
           <button
             className={`w-[160px] h-full text-center border-b-2 border-transparent font-semibold focus:outline-none 
-              ${selectedButton === "판매중" ? "text-black border-black" : "text-light-gray border-light-gray"}`}
+              ${selectedButton === "판매중" ? "text-black border-black border-b-8" : "text-[#9CA3AF]"}`}
             onClick={() => setSelectedButton("판매중")}
           >
             판매중
           </button>
           <button
             className={`w-[160px] h-full text-center border-b-2 border-transparent font-semibold focus:outline-none 
-              ${selectedButton === "판매완료" ? "text-black border-black" : "text-light-gray"}`}
+              ${selectedButton === "판매완료" ? "text-black border-black border-b-8" : "text-[#9CA3AF]"}`}
             onClick={() => setSelectedButton("판매완료")}
           >
             판매완료
@@ -90,19 +92,19 @@ const User = () => {
           <p>총 0 개</p>
           <div>
             <button
-              className={`mr-2 px-2 py-1 rounded-md focus:outline-none ${selectedTab === "최신순" ? "text-black" : "text-light-gray"}`}
+              className={`mr-2 px-2 py-1 rounded-md focus:outline-none ${selectedTab === "최신순" ? "text-black" : "text-[#9CA3AF]"}`}
               onClick={() => setSelectedTab("최신순")}
             >
               최신순
             </button>
             <button
-              className={`mr-2 px-2 py-1 rounded-md focus:outline-none ${selectedTab === "낮은가격순" ? "text-black" : "text-light-gray"}`}
+              className={`mr-2 px-2 py-1 rounded-md focus:outline-none ${selectedTab === "낮은가격순" ? "text-black" : "text-[#9CA3AF]"}`}
               onClick={() => setSelectedTab("낮은가격순")}
             >
               낮은가격순
             </button>
             <button
-              className={`px-2 py-1 rounded-md focus:outline-none ${selectedTab === "높은가격순" ? "text-black" : "text-light-gray"}`}
+              className={`px-2 py-1 rounded-md focus:outline-none ${selectedTab === "높은가격순" ? "text-black" : "text-[#9CA3AF]"}`}
               onClick={() => setSelectedTab("높은가격순")}
             >
               높은가격순
